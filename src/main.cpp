34b3c35a0da63c58e821c7b56206eb3220a88d68
David Meng <davidmenggx@gmail.com>
2026-03-30 12:16:23 -0700

    Get SDL working and output a simple blank screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,49 @@
-#include <iostream>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+constexpr int kWidth{1000};
+constexpr int kHeight{1000};
 
 int main(int argc, char** argv) { 
-  std::cout << "Hello World\n"; 
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    SDL_Log("SDL initialization error: %s", SDL_GetError());
+    return 1;
+  }
+
+  SDL_Window* window{nullptr};
+  SDL_Renderer* renderer{nullptr};
+
+  if (!SDL_CreateWindowAndRenderer("sizzle", kWidth, kWidth, 0, &window, &renderer)) {
+    SDL_Log("Window or renderer error: %s", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  }
+
+  bool running{true};
+  SDL_Event event{};
+
+  while (running) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT || 
+          (event.type == SDL_EVENT_KEY_DOWN && 
+              event.key.key == SDLK_ESCAPE)) {
+        running = false;
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 100, 149, 237, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+
+  window = nullptr;
+  renderer = nullptr;
+
+  SDL_Quit();
 
   return 0;
 }
